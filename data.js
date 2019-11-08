ba48b84fd1abf7ab380ecd8d76cf1604cef9137f
VHHBS <44783931+VHHBS@users.noreply.github.com>
2019-11-08 13:43:19 -0600

    Launch
    Initial launch

---
+++ data.js
@@ -0,0 +1,38 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-56-aw-cathedral",
+      "name": "56 AW Cathedral",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.04489820171286141,
+        "pitch": 0.03545059643152548,
+        "fov": 1.4261791587875234
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "56AW Cathedral",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
